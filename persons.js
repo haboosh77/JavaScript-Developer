f48d5b9f83ee657a0c17e6ac7612346b0f005bd6
Ibtehal AL-Qadri <Ibtehalmo01@gmail.com>
2022-10-05 15:26:48 +0300

    this is the taskes

---
+++ persons.js
@@ -0,0 +1,42 @@
+[
+    {
+        "fname": "Hope",
+        "lname": "Seymour"
+    },
+    {
+        "fname": "Dainius",
+        "lname": "Mcdermott"
+    },
+    {
+        "fname": "Tanya",
+        "lname": "Bondy"
+    },
+    {
+        "fname": "Larisa",
+        "lname": "Laggan"
+    },
+    {
+        "fname": "Luzinda",
+        "lname": "White"
+    },
+    {
+        "fname": "Sheri",
+        "lname": "Fitf"
+    },
+    {
+        "fname": "Marguerite",
+        "lname": "Tupper"
+    },
+    {
+        "fname": "Svetlana",
+        "lname": "Fournier"
+    },
+    {
+        "fname": "Kay",
+        "lname": "Mccarley"
+    },
+    {
+        "fname": "Eva",
+        "lname": "French"
+    }
+]
